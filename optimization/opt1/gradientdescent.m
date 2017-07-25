161ebcf26412772f7296eacc3729626fd14ee198
geeanlooca <marcon.gluca@gmail.com>
2017-07-25 18:41:26 +0200

    updated gradient descent parameters

--- optimization/opt1/gradientdescent.m
+++ optimization/opt1/gradientdescent.m
@@ -3,17 +3,19 @@ set(0,'DefaultFigureWindowStyle','docked')
 clear all;
 
 p = 0.5; % starting point
-gamma = 1e-3; % learning rate
-u = 20; % number of contending stations
+gamma0 = 0.00005; % learning rate
+u = 80; % number of contending stations
 stopping_th = 1e-5;
 
 J = costobj(p, u); % compute initial value of cost function
 
 stop = false;
 iter = 0; % number of iterations
-max_iter = 1500; % maximum number of iterations allowed
+max_iter = 10000; % maximum number of iterations allowed
 
 while stop==false && iter < max_iter
+    
+    gamma = gamma0;% / (iter + 1);
     
     % compute gradient
     grad = 0;
@@ -35,6 +37,7 @@ while stop==false && iter < max_iter
     
     p = p_new;
     iter = iter + 1;
+    
 end
 
 fprintf("Number of contending stations: : %d\n", u);
@@ -47,6 +50,7 @@ Jasd = costobj(p, u);
 hold on;
 plot(p, Jasd, 'k','linewidth',1)
 plot(p_new, J, '-r*','markersize', 12);
+plot([0 1], [J J], '-g');
 hold off;
 xlim([0 1]);
 ylim([0 10]);
